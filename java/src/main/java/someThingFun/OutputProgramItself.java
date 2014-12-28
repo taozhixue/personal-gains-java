43186ebc46884ff980efa783913b33ea22cba5f8
taozhixue <taozx@tt100.cn>
2014-12-28 17:29:41 +0800

    test

---
+++ java/src/main/java/someThingFun/OutputProgramItself.java
@@ -0,0 +1,8 @@
+package someThingFun;
+
+public class OutputProgramItself {
+    public static void main(String[] args) {
+        String s="package someThingFun;public class OutputProgramItself { public static void main(String[] args) {String s=%c%s%c;System.out.printf(s,34,s,34);}}";
+        System.out.printf(s,34,s,34);
+    }
+}
